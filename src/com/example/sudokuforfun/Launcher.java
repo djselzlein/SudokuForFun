0f1c9ce14f2e97d8d81c72b98aa6a1c7e206c4a3
djeisonselzlein <djeisonaguir@gmail.com>
2015-08-27 23:25:52 -0300

    Removed memCachierAddOn

--- src/com/example/sudokuforfun/Launcher.java
+++ src/com/example/sudokuforfun/Launcher.java
@@ -2,14 +2,11 @@ package com.example.sudokuforfun;
 
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
-import static eu.hurion.vaadin.heroku.MemcachedManagerBuilder.memcachierAddOn;
 
 public class Launcher {
 
 	public static void main(final String[] args) {
-	    herokuServer(forApplication(SudokuforfunUI.class))
-	            .withMemcachedSessionManager(memcachierAddOn())
-	            .start();
+	    herokuServer(forApplication(SudokuforfunUI.class)).start();
 	}
 	
 }
